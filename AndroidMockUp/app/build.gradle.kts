17b72d8fa970477ccefacf310106328c4c6c65e1
Mihai Doinea <mihai.doinea@oracle.com>
2023-11-18 14:22:58 +0200

    Android Mockup

--- AndroidMockUp/app/build.gradle.kts
+++ AndroidMockUp/app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.flt.checklist"
-        minSdk = 20
+        minSdk = 26
         targetSdk = 33
         versionCode = 1
         versionName = "1.0"
